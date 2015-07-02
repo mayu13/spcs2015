a9dc2a6afad0a6396048d2893dd6338efd36527f
mayu13 <mayurivachhani987@gmail.com>
2015-07-02 11:12:40 +0530

    spcsusa

---
+++ www/js/app.js
@@ -0,0 +1,265 @@
+
+angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', 'ngSanitize', 'ngCordova'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    
+    if (window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if (window.StatusBar) {
+        StatusBar.styleDefault();
+    }
+  });
+})
+
+
+//.controller('AccountCtrl', function($scope) {
+ //$scope.myHTML = '<a href="http://www.spcsusa.org/2015/texas/convention/welcome.html" name="abc">Click Here Go To Our Website</a> ';
+//})
+//.controller('RegiCtrl', function($scope) {
+ //$scope.myHTML1 = '<center><a href="http://www.spcsusa.org/2015/texas/convention/allregistration"><b>Click here For Registration</b></a> </center>';
+//})
+
+.config(function($stateProvider, $urlRouterProvider, $ionicConfigProvider) {
+ 
+  $ionicConfigProvider.tabs.position('bottom');
+
+  $stateProvider
+    .state('tab', {
+    url: "/tab",
+    abstract: true,
+    templateUrl: "templates/tabs.html"
+  })
+  .state('tab.dash', {
+    url: '/dash',
+    views: {
+      'tab-dash': {
+        templateUrl: 'templates/tab-dash.html',
+        controller: 'DashCtrl'
+      }
+    }
+  })
+  .state('tab.about', {
+    url: '/about',
+    views: {
+      'tab-dash': {
+        templateUrl: 'templates/about.html'        
+      }
+    }
+  })
+  .state('tab.schedule', {
+    url: '/schedule',
+    views: {
+      'tab-dash': {
+        templateUrl: 'templates/schedule.html'        
+      }
+    }
+  })
+   .state('tab.sch_pdf_image', {
+    url: '/sch_pdf_image',
+    views: {
+      'tab-dash': {
+        templateUrl: 'templates/sch_pdf_image.html'        
+      }
+    }
+  })
+  
+  .state('tab.activity', {
+    url: '/activity',
+    views: {
+      'tab-dash': {
+        templateUrl: 'templates/activity.html'        
+      }
+    }
+  })
+    .state('tab.1act_perot', {
+    url: '/1act_perot',
+    views: {
+      'tab-dash': {
+        templateUrl: 'templates/1act_perot.html'        
+      }
+    }
+  })
+    .state('tab.2act_park', {
+    url: '/2act_park',
+    views: {
+      'tab-dash': {
+        templateUrl: 'templates/2act_park.html'        
+      }
+    }
+  })
+    .state('tab.3act_group', {
+    url: '/3act_group',
+    views: {
+      'tab-dash': {
+        templateUrl: 'templates/3act_group.html'        
+      }
+    }
+  })
+    .state('tab.4act_seniour', {
+    url: '/4act_seniour',
+    views: {
+      'tab-dash': {
+        templateUrl: 'templates/4act_seniour.html'        
+      }
+    }
+  })
+   
+   .state('tab.texas_spcs', {
+    url: '/texas_spcs',
+    views: {
+      'tab-dash': {
+        templateUrl: 'templates/texas_spcs.html'        
+      }
+    }
+  })
+  .state('tab.committe', {
+    url: '/committe',
+    views: {
+      'tab-dash': {
+        templateUrl: 'templates/committe.html'        
+      }
+    }
+  })
+  .state('tab.c_advisory_team', {
+    url: '/c_advisory_team',
+    views: {
+      'tab-dash': {
+        templateUrl: 'templates/c_advisory_team.html'        
+      }
+    }
+  })
+  .state('tab.c_chapter_team', {
+    url: '/c_chapter_team',
+    views: {
+      'tab-dash': {
+        templateUrl: 'templates/c_chapter_team.html'        
+      }
+    }
+  })
+  .state('tab.c_steering_team', {
+    url: '/c_steering_team',
+    views: {
+      'tab-dash': {
+        templateUrl: 'templates/c_steering_team.html'        
+      }
+    }
+  })
+  .state('tab.c_volunteer_team', {
+    url: '/c_volunteer_team',
+    views: {
+      'tab-dash': {
+        templateUrl: 'templates/c_volunteer_team.html'        
+      }
+    }
+  })
+   
+   .state('tab.sponsor', {
+    url: '/sponsor',
+    views: {
+      'tab-dash': {
+        templateUrl: 'templates/sponsor.html'        
+      }
+    }
+  })
+  
+  .state('tab.regi', {
+    url: '/regi',
+    views: {
+      'tab-dash': {
+        templateUrl: 'templates/regi.html'        
+      }
+    }
+  })
+   
+    .state('tab.gallery', {
+    url: '/gallery',
+    views: {
+      'tab-dash': {
+        templateUrl: 'templates/gallery.html'        
+      }
+    }
+  })
+  
+  .state('tab.news', {
+    url: '/news',
+    views: {
+      'tab-dash': {
+        templateUrl: 'templates/news.html'        
+      }
+    }
+  })
+  
+    .state('tab.reach', {
+    url: '/reach',
+    views: {
+      'tab-dash': {
+        templateUrl: 'templates/reach.html'        
+      }
+    }
+  })
+   .state('tab.venue', {
+    url: '/venue',
+    views: {
+      'tab-dash': {
+        templateUrl: 'templates/venue.html'        
+      }
+    }
+  })
+  .state('tab.r_map', {
+    url: '/r_map',
+    views: {
+      'tab-dash': {
+        templateUrl: 'templates/r_map.html'        
+      }
+    }
+  })
+  .state('tab.r_airways', {
+    url: '/r_airways',
+    views: {
+      'tab-dash': {
+        templateUrl: 'templates/r_airways.html'        
+      }
+    }
+  })
+  .state('tab.r_city_guide', {
+    url: '/r_city_guide',
+    views: {
+      'tab-dash': {
+        templateUrl: 'templates/r_city_guide.html'        
+      }
+    }
+  })
+  
+  .state('tab.account', {
+    url: '/account',
+    views: {
+      'tab-account': {
+        templateUrl: 'templates/tab-account.html',
+        controller: 'AccountCtrl'
+      }
+    }
+  })
+  .state('tab.chats', {
+      url: '/chats',
+      views: {
+        'tab-chats': {
+          templateUrl: 'templates/tab-chats.html',
+          controller: 'ChatsCtrl'
+        }
+      }
+    })
+	.state('tab.member', {
+    url: '/member',
+    views: {
+      'tab-member': {
+        templateUrl: 'templates/tab-member.html',
+        controller: 'MemberCtrl'
+      }
+    }
+  });
+   
+  $urlRouterProvider.otherwise('/tab/dash');
+
+});
---
+++ www/js/controllers.js
@@ -0,0 +1,432 @@
+angular.module('starter.controllers', [])
+
+.controller('DashCtrl', function($scope, $http) {
+   $http.get("http://www.techroversolutions.com/Ionic_Mayu/fetch.php")
+   .success(function (response) {$scope.names = response.records;});
+  
+})
+.controller('PdfCtrl', function($scope, $cordovaFileOpener2) {
+
+  $cordovaFileOpener2.open(
+    '/img/pdf_sch.pdf',
+    'application/pdf'
+  ).then(function() {
+      // file opened successfully
+  }, function(err) {
+      // An error occurred. Show a message to the user
+  });
+
+})
+
+
+
+  .controller('MapCtrl', function($scope, $ionicLoading, $compile)
+  {
+      function initialize()
+	  {
+        var myLatlng = new google.maps.LatLng(43.07493,-89.381388);
+       
+		
+        var mapOptions = 
+		{
+          center: myLatlng,
+          zoom: 16,
+          mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+        var map = new google.maps.Map(document.getElementById("map"),
+            mapOptions);
+        
+        //Marker + infowindow + angularjs compiled ng-click
+        var contentString = "<div><a ng-click='clickTest()'>Click me!</a></div>";
+        var compiled = $compile(contentString)($scope);
+
+        var infowindow = new google.maps.InfoWindow({
+          content: compiled[0]
+        });
+
+        var marker = new google.maps.Marker({
+          position: myLatlng,
+          map: map,
+          title: 'Uluru (Ayers Rock)'
+        });
+
+        google.maps.event.addListener(marker, 'click', function() {
+          infowindow.open(map,marker);
+        });
+
+        $scope.map = map;
+      }
+      google.maps.event.addDomListener(window, 'load', initialize);
+      
+      $scope.centerOnMe = function() 
+	  {
+        if(!$scope.map)
+		{
+          return;
+        }
+
+        $scope.loading = $ionicLoading.show
+		({
+          content: 'Getting current location...',
+          showBackdrop: false
+        });
+
+        navigator.geolocation.getCurrentPosition(function(pos) 
+		{
+          $scope.map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
+          $scope.loading.hide();
+        }, function(error)
+		{
+          alert('Unable to get location: ' + error.message);
+        });
+		
+      };
+	  
+      
+      $scope.clickTest = function()
+	  {
+        alert('Example of infowindow with ng-click')
+      };
+      
+    })
+
+
+
+.controller('MailCtrl', function($scope, $cordovaSocialSharing){
+
+	$scope.shareAnywhere = function()
+	{
+	 $cordovaSocialSharing.share("Click Here To Download SPCSUSA App...", "For SPCSUSA App..", null, "https://play.google.com/store/apps/details?id=com.ionicframework.spcsmayu334368");
+	// $cordovaSocialSharing.share("This is a message to share...hellooo", "subject.........", "null...attchement", "http://blog.nraboy.com");
+	}
+	
+	
+	 $scope.shareViaTwitter = function(message, image, link) {
+     $cordovaSocialSharing
+       .shareViaTwitter(message, image, link)
+       .then(function(result) {
+         // Success!
+         alert("success : "+result);
+       }, function(err) {
+         // An error occurred. Show a message to the user
+         alert("Cannot share on Twitter");
+       });
+   }
+
+	
+  $scope.shareViaWhatsApp = function(message, image, link) {
+     $cordovaSocialSharing
+       .shareViaWhatsApp(message, image, link)
+       .then(function(result) {
+       
+         // Success!
+       }, function(err) {
+         // An error occurred. Show a message to the user
+          alert("Cannot share on WhatsApp");
+       });
+   }
+   
+   
+   $scope.shareViaFacebook = function(message, image, link) {
+     $cordovaSocialSharing
+       .shareViaFacebook(message, image, link)
+       .then(function(result) {
+         // Success!
+       }, function(err) {
+         // An error occurred. Show a message to the user
+         alert("Cannot share on Facebook");
+       });
+   }
+ 
+
+	
+	
+})
+
+
+
+.controller('SplashCtrl', function($scope, $cordovaSplashscreen) {
+  $cordovaSplashscreen.show();
+})
+
+.controller('AccountCtrl', function($scope) {
+})
+
+.controller('ChatsCtrl', function($scope) {
+  
+})
+
+.controller('MemberCtrl', function($scope) {
+  
+})
+
+
+.controller('DetailCtrl', function($scope, $ionicSlideBoxDelegate) {
+   $scope.navSlide = function(index) {
+        $ionicSlideBoxDelegate.slide(index, 500);
+    }
+})
+
+.controller('CommitteCtrl', function($scope, $http) {
+   $http.get("http://www.techroversolutions.com/Ionic_Mayu/fetch_member.php")
+   .success(function (response) {$scope.names = response.records;});
+})
+
+.controller('AdvisoryCtrl', function($scope, $http) {
+   $http.get("http://www.techroversolutions.com/Ionic_Mayu/advisory_team.php")
+   .success(function (response) {$scope.names = response.records;});
+})
+
+.controller('ChaptersCtrl', function($scope, $http) {
+   $http.get("http://www.techroversolutions.com/Ionic_Mayu/chapter_team.php")
+   .success(function (response) {$scope.names = response.records;});
+})
+
+.controller('SchCtrl', function($scope, $http) {
+   $http.get("http://www.techroversolutions.com/Ionic_Mayu/sch.php")
+   .success(function (response) {$scope.names = response.records;});
+})
+
+.controller('CoCtrl', function($scope, $http) {
+    $http.get("http://www.techroversolutions.com/Ionic_Mayu/v_food.php")
+   .success(function (response) {$scope.names = response.records;});
+})
+.controller('DecoCtrl', function($scope, $http) {
+    $http.get("http://www.techroversolutions.com/Ionic_Mayu/v_deco.php")
+   .success(function (response) {$scope.names = response.records;});
+})
+
+.controller('DirCtrl', function($scope, $http) {
+    $http.get("http://www.techroversolutions.com/Ionic_Mayu/v_dir.php")
+   .success(function (response) {$scope.names = response.records;});
+})
+
+
+
+.controller('SafCtrl', function($scope, $http) {
+    $http.get("http://www.techroversolutions.com/Ionic_Mayu/v_safety.php")
+   .success(function (response) {$scope.names = response.records;});
+})
+
+.controller('GenCtrl', function($scope, $http) {
+    $http.get("http://www.techroversolutions.com/Ionic_Mayu/v_gen.php")
+   .success(function (response) {$scope.names = response.records;});
+})
+
+.controller('HealthCtrl', function($scope, $http) {
+    $http.get("http://www.techroversolutions.com/Ionic_Mayu/v_health.php")
+   .success(function (response) {$scope.names = response.records;});
+})
+
+.controller('HosCtrl', function($scope, $http) {
+    $http.get("http://www.techroversolutions.com/Ionic_Mayu/v_hos.php")
+   .success(function (response) {$scope.names = response.records;});
+})
+
+.controller('KidsCtrl', function($scope, $http) {
+    $http.get("http://www.techroversolutions.com/Ionic_Mayu/v_kids.php")
+   .success(function (response) {$scope.names = response.records;});
+})
+.controller('OutCtrl', function($scope, $http) {
+    $http.get("http://www.techroversolutions.com/Ionic_Mayu/v_out.php")
+   .success(function (response) {$scope.names = response.records;});
+})
+
+.controller('ProgCtrl', function($scope, $http) {
+    $http.get("http://www.techroversolutions.com/Ionic_Mayu/v_program.php")
+   .success(function (response) {$scope.names = response.records;});
+})
+
+
+.controller('RelCtrl', function($scope, $http) {
+    $http.get("http://www.techroversolutions.com/Ionic_Mayu/v_pub_rel.php")
+   .success(function (response) {$scope.names = response.records;});
+})
+
+.controller('LostCtrl', function($scope, $http) {
+    $http.get("http://www.techroversolutions.com/Ionic_Mayu/v_reg_lost.php")
+   .success(function (response) {$scope.names = response.records;});
+})
+
+
+.controller('SenCtrl', function($scope, $http) {
+    $http.get("http://www.techroversolutions.com/Ionic_Mayu/v_sen.php")
+   .success(function (response) {$scope.names = response.records;});
+})
+
+.controller('SponserCtrl', function($scope, $http) {
+    $http.get("http://www.techroversolutions.com/Ionic_Mayu/v_sponser.php")
+   .success(function (response) {$scope.names = response.records;});
+})
+
+.controller('TransCtrl', function($scope, $http) {
+    $http.get("http://www.techroversolutions.com/Ionic_Mayu/v_trans.php")
+   .success(function (response) {$scope.names = response.records;});
+})
+
+.controller('YouthCtrl', function($scope, $http) {
+    $http.get("http://www.techroversolutions.com/Ionic_Mayu/v_youth.php")
+   .success(function (response) {$scope.names = response.records;});
+})
+.controller('BudgetCtrl', function($scope, $http) {
+    $http.get("http://www.techroversolutions.com/Ionic_Mayu/v_budget.php")
+   .success(function (response) {$scope.names = response.records;});
+})
+.controller('PhotoCtrl', function($scope, $http) {
+    $http.get("http://www.techroversolutions.com/Ionic_Mayu/v_photo.php")
+   .success(function (response) {$scope.names = response.records;});
+})
+
+.controller('WomenCtrl', function($scope, $http) {
+    $http.get("http://www.techroversolutions.com/Ionic_Mayu/v_women.php")
+   .success(function (response) {$scope.names = response.records;});
+})
+
+.controller('StandCtrl', function($scope, $http) {
+    $http.get("http://www.techroversolutions.com/Ionic_Mayu/v_women.php")
+   .success(function (response) {$scope.names = response.records;});
+})
+
+
+
+
+
+.controller('SchhCtrl', function($scope, $http) {
+   $http.get("http://www.techroversolutions.com/Ionic_Mayu/sch_two.php")
+   .success(function (response) {$scope.names = response.records;});
+})
+
+.controller('SchhhCtrl', function($scope, $http) {
+   $http.get("http://www.techroversolutions.com/Ionic_Mayu/sch_thre.php")
+   .success(function (response) {$scope.names = response.records;});
+})
+
+.controller('SchlastCtrl', function($scope, $http) {
+   $http.get("http://www.techroversolutions.com/Ionic_Mayu/sch_last.php")
+   .success(function (response) {$scope.names = response.records;});
+})
+
+.controller('NewsCtrl', function($scope, $http) {
+   $http.get("http://www.techroversolutions.com/Ionic_Mayu/news.php")
+   .success(function (response) {$scope.names = response.records;});
+})
+
+
+
+
+.controller('SponsorCtrl', function($scope)
+{
+	$scope.images = [];
+	$scope.loadImages = function() {
+	
+			$scope.images.push({ src: "img/logo1.png"});
+		
+		
+			$scope.images.push({ src: "img/sec.jpg"});
+			$scope.images.push({ src: "img/logo3.jpg"});
+			
+			$scope.images.push({ src: "img/multi_logo.jpg"});
+			$scope.images.push({ src: "img/bank.png"});
+			$scope.images.push({ src: "img/horizon.jpg"});
+			
+		
+	}
+		
+})
+.controller('SchPicCtrl', function($scope){
+	$scope.images = [];
+	$scope.loadImages = function() {
+		
+			$scope.images.push({ src: "img/sch/ProGui4_Page_1.jpeg"});
+			$scope.images.push({ src: "img/sch/ProGui4_Page_2.jpeg"});
+			$scope.images.push({ src: "img/sch/ProGui4_Page_3.jpeg"});
+			$scope.images.push({ src: "img/sch/ProGui4_Page_4.jpeg"});
+			$scope.images.push({ src: "img/sch/ProGui4_Page_5.jpeg"});
+			$scope.images.push({ src: "img/sch/ProGui4_Page_6.jpeg"});
+			$scope.images.push({ src: "img/sch/ProGui4_Page_7.jpeg"});
+			$scope.images.push({ src: "img/sch/ProGui4_Page_8.jpeg"});
+			$scope.images.push({ src: "img/sch/ProGui4_Page_9.jpeg"});
+			$scope.images.push({ src: "img/sch/ProGui4_Page_10.jpeg"});
+			$scope.images.push({ src: "img/sch/ProGui4_Page_11.jpeg"});
+			$scope.images.push({ src: "img/sch/ProGui4_Page_12.jpeg"});
+			$scope.images.push({ src: "img/sch/ProGui4_Page_13.jpeg"});
+			$scope.images.push({ src: "img/sch/ProGui4_Page_14.jpeg"});
+			$scope.images.push({ src: "img/sch/ProGui4_Page_15.jpeg"});
+			$scope.images.push({ src: "img/sch/ProGui4_Page_16.jpeg"});
+			$scope.images.push({ src: "img/sch/ProGui4_Page_17.jpeg"});
+			$scope.images.push({ src: "img/sch/ProGui4_Page_18.jpeg"});
+			$scope.images.push({ src: "img/sch/ProGui4_Page_19.jpeg"});
+			$scope.images.push({ src: "img/sch/ProGui4_Page_20.jpeg"});
+			$scope.images.push({ src: "img/sch/ProGui4_Page_21.jpeg"});
+			$scope.images.push({ src: "img/sch/ProGui4_Page_22.jpeg"});
+			$scope.images.push({ src: "img/sch/ProGui4_Page_23.jpeg"});
+			$scope.images.push({ src: "img/sch/ProGui4_Page_24.jpeg"});
+			$scope.images.push({ src: "img/sch/ProGui4_Page_25.jpeg"});
+			$scope.images.push({ src: "img/sch/ProGui4_Page_26.jpeg"});
+			$scope.images.push({ src: "img/sch/ProGui4_Page_27.jpeg"});
+			$scope.images.push({ src: "img/sch/ProGui4_Page_28.jpeg"});
+			$scope.images.push({ src: "img/sch/ProGui4_Page_29.jpeg"});
+			$scope.images.push({ src: "img/sch/ProGui4_Page_30.jpeg"});
+			$scope.images.push({ src: "img/sch/ProGui4_Page_31.jpeg"});
+			$scope.images.push({ src: "img/sch/ProGui4_Page_32.jpeg"});
+			$scope.images.push({ src: "img/sch/ProGui4_Page_33.jpeg"});
+			$scope.images.push({ src: "img/sch/ProGui4_Page_34.jpeg"});
+			$scope.images.push({ src: "img/sch/ProGui4_Page_35.jpeg"});
+			$scope.images.push({ src: "img/sch/ProGui4_Page_36.jpeg"});
+			$scope.images.push({ src: "img/sch/ProGui4_Page_37.jpeg"});
+			$scope.images.push({ src: "img/sch/ProGui4_Page_38.jpeg"});
+			$scope.images.push({ src: "img/sch/ProGui4_Page_39.jpeg"});
+			$scope.images.push({ src: "img/sch/ProGui4_Page_40.jpeg"});
+			$scope.images.push({ src: "img/sch/ProGui4_Page_41.jpeg"});
+			$scope.images.push({ src: "img/sch/ProGui4_Page_42.jpeg"});
+			$scope.images.push({ src: "img/sch/ProGui4_Page_43.jpeg"});
+			$scope.images.push({ src: "img/sch/ProGui4_Page_44.jpeg"});
+			$scope.images.push({ src: "img/sch/ProGui4_Page_45.jpeg"});
+			$scope.images.push({ src: "img/sch/ProGui4_Page_46.jpeg"});
+			$scope.images.push({ src: "img/sch/ProGui4_Page_47.jpeg"});
+			$scope.images.push({ src: "img/sch/ProGui4_Page_48.jpeg"});
+			
+				
+				
+		
+	}
+})
+
+
+.controller('MediaCtrl', function($scope){
+	$scope.images = [];
+	$scope.loadImages = function() {
+		
+			$scope.images.push({ src: "img/about_1.jpg"});
+			$scope.images.push({ src: "img/about_2.jpg"});
+			$scope.images.push({ src: "img/about_3.jpg"});
+			$scope.images.push({ src: "img/act_6.jpg"});			
+			$scope.images.push({ src: "img/hotel_7.png"});
+			$scope.images.push({ src: "img/act_1.jpg"});
+			$scope.images.push({ src: "img/act_2.jpg"});
+			$scope.images.push({ src: "img/act_3.png"});
+			$scope.images.push({ src: "img/act_5.jpg"});			
+			$scope.images.push({ src: "img/c7.jpg"});
+			$scope.images.push({ src: "img/act_11.jpg"});
+			$scope.images.push({ src: "img/act_13.jpg"});
+			$scope.images.push({ src: "img/hotel_5.jpg"});
+			$scope.images.push({ src: "img/p_sch_1.jpg"});
+			$scope.images.push({ src: "img/p_sch_3.jpg"});
+			$scope.images.push({ src: "img/p_sch_2.png"});			
+			$scope.images.push({ src: "img/hotel_8.png"});
+			$scope.images.push({ src: "img/hotel_4.png"});
+			$scope.images.push({ src: "img/act_10.jpg"});
+			$scope.images.push({ src: "img/hotel_6.png"});
+			$scope.images.push({ src: "img/c8.jpg"});
+			$scope.images.push({ src: "img/c13.jpg"});
+			$scope.images.push({ src: "img/car1.jpg"});
+			$scope.images.push({ src: "img/c14.jpg"});			
+		
+	}
+});
+
+
+
+
+
+
